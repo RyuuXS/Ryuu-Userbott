28effb69f8405346273c2825411e42bd8469bb8f
AbiantXchuan <96214088+RyuuXS@users.noreply.github.com>
2022-01-16 06:01:30 +0700

    Update www.py

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -184,14 +184,14 @@ async def redis(pong):
     """For .ping command, ping the userbot from any chat."""
     uptime = await get_readable_time((time.time() - StartTime))
     start = datetime.now()
-    await pong.edit("**✗**")
-    await pong.edit("**✗✗**")
-    await pong.edit("**✗✗✗**")
-    await pong.edit("**✗‿✗ PONG!**")
+    await pong.edit("**𖣘**")
+    await pong.edit("**𖣘𖣘**")
+    await pong.edit("**𖣘𖣘𖣘**")
+    await pong.edit("**🖕‿🖕 PONG!**")
     end = datetime.now()
     duration = (end - start).microseconds / 1000
     await pong.edit(
-        f"**🗿DELAY KAN!!**\n"
+        f"**🗿DELAY KAN BLOKK!!**\n"
         f"➠ **ᴘɪɴɢ:** "
         f"`%sms` \n"
         f"➠ **ᴜᴘᴛɪᴍᴇ:** "
