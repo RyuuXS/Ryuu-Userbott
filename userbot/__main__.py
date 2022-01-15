d517262760c545be1ae9f3634529b7242b3c3e3e
AbiantXchuan <96214088+RyuuXS@users.noreply.github.com>
2022-01-15 16:19:24 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -28,7 +28,7 @@ for module_name in ALL_MODULES:
     imported_module = import_module("userbot.modules." + module_name)
 
 
-LOGS.info(f"⚡Kyura - Userbot⚡ ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN KONTOLL!!!]")
+LOGS.info(f"༒︎Ryuu - Userbott༒︎ ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN KONTOLL MEMEKS!!!]")
 
 
 if len(argv) not in (1, 3, 4):
