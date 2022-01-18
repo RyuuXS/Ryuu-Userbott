d224da03e030a3f0e89ffada5b4169b5d550c1a0
AbiantXchuan <96214088+RyuuXS@users.noreply.github.com>
2022-01-19 06:40:45 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -38,7 +38,7 @@ async def help(event):
             "**🗿𝑹𝒀𝑼𝑼-𝑼𝑺𝑬𝑹𝑩𝑶𝑻🗿**\n\n"
             f"**◉ Bᴏᴛ ᴏꜰ {DEFAULTUSER}**\n**◉ Mᴏᴅᴜʟᴇꜱ : {len(modules)}**\n\n"
             "**• Mᴀɪɴ Mᴇɴᴜ :**\n"
-            f"◉ {string}◉\n\n༆ **ɴᴏᴛᴇꜱ :**  `.help animasi`\n༆  sᴜᴘᴘᴏʀᴛ :  @RYUUSHINNI"
+            f"◉ {string}◉\n\n༆ **ɴᴏᴛᴇꜱ :**  `.help animasi`\n༆  sᴜᴘᴘᴏʀᴛ :  **[ᴅᴇᴠ](https://t.me/RYUUSHINNI)**"
         )
         await asyncio.sleep(1000)
         await event.delete()
