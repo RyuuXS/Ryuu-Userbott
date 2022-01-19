97e370c235199d2d5c0d2cac821bd5742f52cca7
Ryuu <daudilham0606@gmail.com>
2022-01-19 02:40:47 +0000

    Ryuu-Userbott: auto fixes
    Signed-off-by: Ryuu <daudilham0606@gmail.com>

--- userbot/modules/Animasi9.py
+++ userbot/modules/Animasi9.py
@@ -5,70 +5,79 @@ from userbot import CMD_HELP
 from userbot.events import register
 
 
-@register(outgoing=True, pattern='^.wlcm(?: |$)(.*)')
+@register(outgoing=True, pattern="^.wlcm(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("───▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄───\n"
-                     "───█▒▒░░░░░░░░░▒▒█───\n"
-                     "────█░░█░░░░░█░░█────\n"
-                     "─▄▄──█░░░▀█▀░░░█──▄▄─\n"
-                     "█░░█─▀▄░░░░░░░▄▀─█░░█\n"
-                     "█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█\n"
-                     "█░░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░░█\n"
-                     "█░░║║║╠─║─║─║║║║║╠─░░█\n"
-                     "█░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█\n"
-                     "█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█\n")
-    
-    
-@register(outgoing=True, pattern='^.wlc(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█\n"
-                     "█░░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░░█\n"
-                     "█░░║║║╠─║─║─║║║║║╠─░░█\n"
-                     "█░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█\n"
-                     "█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█\n")
-                       
-    
-@register(outgoing=True, pattern='^.klb(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("   ╚⊙ ⊙╝..\n"
-                     "   ╚═(███)═╝\n"
-                     "    ╚═(███)═╝\n"
-                     "       ╚═(███)═╝\n"
-                     "        ╚═(███)═╝\n"
-                     "        ╚═(███)═╝\n"
-                     "        ╚═(███)═╝\n"
-                     "      ╚═(███)═╝\n"
-                     "     ╚═(███)═╝\n"
-                     "    ╚═(███)═╝ \n"                       
-                     "    ╚═(███)═╝\n"
-                     "     ╚═(███)═╝\n"
-                     "      ╚═(███)═╝\n"
-                     "       ╚═(███)═╝\n"
-                     "        ╚═(███)═╝\n"
-                     "        ╚═(███)═╝\n"
-                     "       ╚═(███)═╝\n"
-                     "      ╚═(███)═╝\n"
-                     "     ╚═(███)═╝\n"
-                     "     ╚═(███)═╝\n"
-                     "      ╚═(█)═╝\n")
-                     
+    await typew.edit(
+        "───▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄───\n"
+        "───█▒▒░░░░░░░░░▒▒█───\n"
+        "────█░░█░░░░░█░░█────\n"
+        "─▄▄──█░░░▀█▀░░░█──▄▄─\n"
+        "█░░█─▀▄░░░░░░░▄▀─█░░█\n"
+        "█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█\n"
+        "█░░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░░█\n"
+        "█░░║║║╠─║─║─║║║║║╠─░░█\n"
+        "█░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█\n"
+        "█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█\n"
+    )
 
-@register(outgoing=True, pattern='^.lari(?: |$)(.*)')
+
+@register(outgoing=True, pattern="^.wlc(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("──▄█▀█▄─────────██\n"
-                     "▄████████▄───▄▀█▄▄▄▄\n"
-                     "██▀▼▼▼▼▼─▄▀──█▄▄\n"
-                     "█████▄▲▲▲─▄▄▄▀───▀▄\n"
-                     "██████▀▀▀▀─▀────────▀▀\n"
-                     " **LARI IPINNN** \n")
-    
-CMD_HELP.update({
-    "animasi9":
-    "`.wlcm` ; `.wlc` ; `.klb` ; `.lari`\
+    await typew.edit(
+        "█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█\n"
+        "█░░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░░█\n"
+        "█░░║║║╠─║─║─║║║║║╠─░░█\n"
+        "█░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█\n"
+        "█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█\n"
+    )
+
+
+@register(outgoing=True, pattern="^.klb(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit(
+        "   ╚⊙ ⊙╝..\n"
+        "   ╚═(███)═╝\n"
+        "    ╚═(███)═╝\n"
+        "       ╚═(███)═╝\n"
+        "        ╚═(███)═╝\n"
+        "        ╚═(███)═╝\n"
+        "        ╚═(███)═╝\n"
+        "      ╚═(███)═╝\n"
+        "     ╚═(███)═╝\n"
+        "    ╚═(███)═╝ \n"
+        "    ╚═(███)═╝\n"
+        "     ╚═(███)═╝\n"
+        "      ╚═(███)═╝\n"
+        "       ╚═(███)═╝\n"
+        "        ╚═(███)═╝\n"
+        "        ╚═(███)═╝\n"
+        "       ╚═(███)═╝\n"
+        "      ╚═(███)═╝\n"
+        "     ╚═(███)═╝\n"
+        "     ╚═(███)═╝\n"
+        "      ╚═(█)═╝\n"
+    )
+
+
+@register(outgoing=True, pattern="^.lari(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit(
+        "──▄█▀█▄─────────██\n"
+        "▄████████▄───▄▀█▄▄▄▄\n"
+        "██▀▼▼▼▼▼─▄▀──█▄▄\n"
+        "█████▄▲▲▲─▄▄▄▀───▀▄\n"
+        "██████▀▀▀▀─▀────────▀▀\n"
+        " **LARI IPINNN** \n"
+    )
+
+
+CMD_HELP.update(
+    {
+        "animasi9": "`.wlcm` ; `.wlc` ; `.klb` ; `.lari`\
     \nUsage: liat aja."
-})                     
-                   
+    }
+)
