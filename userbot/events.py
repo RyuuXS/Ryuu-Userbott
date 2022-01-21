4e46149ee3569037c90128f1d4a453faecc77c4f
Kayzu <96322205+Kayzyu@users.noreply.github.com>
2022-01-21 21:42:11 +0700

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -96,11 +96,11 @@ def register(**args):
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-                    text = "**Kayzu-Ubot ERROR**\n"
-                    link = "Silahkan chat: @Kayzuuuuu"
+                    text = "**Ryuu-Userbot ERROR**\n"
+                    link = "Silahkan chat: @RYUUSHINNI"
                     text += "Untuk melaporkan kesalahan"
                     text += f"tinggal teruskan pesan ini {link}.\n"
-                    text += "Kayzu Siap Membantu Kamu\n"
+                    text += "Ryuu Siap Membantu Kamu\n"
 
                     ftext = "========== DISCLAIMER =========="
                     ftext += "\nThis file uploaded ONLY here,"
