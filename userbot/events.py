1dafc807ca166fafcee56ea8f0b42fa5f48b8691
Ryuu <daudilham0606@gmail.com>
2022-01-22 03:03:22 +0000

    Ryuu-Userbott: auto fixes
    Signed-off-by: Ryuu <daudilham0606@gmail.com>

--- userbot/events.py
+++ userbot/events.py
@@ -144,5 +144,5 @@ def register(**args):
             bot.add_event_handler(wrapper, events.MessageEdited(**args))
         bot.add_event_handler(wrapper, events.NewMessage(**args))
         return wrapper
-       
+
     return decorator
