80de5bfe24e3799534bd5ef81542e7523318898d
Ryuu <daudilham0606@gmail.com>
2022-01-22 04:33:27 +0000

    Ryuu-Userbott: auto fixes
    Signed-off-by: Ryuu <daudilham0606@gmail.com>

--- userbot/modules/kenalan.py
+++ userbot/modules/kenalan.py
@@ -1,8 +1,9 @@
 from time import sleep
+
 from userbot.events import register
 
 
-@register(outgoing=True, pattern='^.halo(?: |$)(.*)')
+@register(outgoing=True, pattern="^.halo(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(1)
@@ -11,10 +12,12 @@ async def typewriter(typew):
     await typew.edit("17 Tahun`")
     sleep(1)
     await typew.edit("`Tinggal Di JATIM, Salam Kenal:)`")
+
+
 # Create by myself @localheart
 
 
-@register(outgoing=True, pattern='^.sayong(?: |$)(.*)')
+@register(outgoing=True, pattern="^.sayong(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(3)
@@ -23,10 +26,12 @@ async def typewriter(typew):
     await typew.edit("`Aku Sayang Kamu`")
     sleep(1)
     await typew.edit("`I LOVE YOU 💞`")
+
+
 # Create by myself @localheart
 
 
-@register(outgoing=True, pattern='^.semangat(?: |$)(.*)')
+@register(outgoing=True, pattern="^.semangat(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(3)
@@ -35,4 +40,6 @@ async def typewriter(typew):
     await typew.edit("`Tetaplah Bernapas`")
     sleep(1)
     await typew.edit("`Dan Selalu Bersyukur`")
+
+
 # Create by myself @localheart
